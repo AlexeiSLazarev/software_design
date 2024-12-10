4ab5116ee2e1acba4d64d4beff76f1b39a97d5cd
AlexeiSLazarev <alexei.s.lazarev@gmail.com>
2024-12-11 00:11:16 +0300

    - new lesson

---
+++ demo/src/main/java/RaceConditionExample.java
@@ -0,0 +1,34 @@
+// 1. Итоговое значение счетчика неверно из-за того что переменная не является атомарной 
+// -> Оба потока сразу ее изменяют и изменения перекрываются.
+// Исправленный вариант с атомарной переменной
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class RaceConditionExample {
+
+    private static AtomicInteger counter = new AtomicInteger(0);
+
+    public static void main(String[] args) {
+        int numberOfThreads = 10;
+        Thread[] threads = new Thread[numberOfThreads];
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            threads[i] = new Thread(() -> {
+                for (int j = 0; j < 100000; j++) {
+                    counter.incrementAndGet();
+                }
+            });
+            threads[i].start();
+        }
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Final counter value: " + counter.get());
+    }
+}
